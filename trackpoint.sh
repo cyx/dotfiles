422f07ca93acf4fec9709c8bcbb1a797c769444c
Cyril David <cyx@cyx.is>
2015-08-24 13:24:51 -0700

    Update trackpoint settings

--- trackpoint.sh
+++ trackpoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Use this in conjunction with hi dpi type laptops. I wasn't
-# able to find any other option for making the trackpoint fast
-# enough in this context other than the serio1 hack.
-echo -n 200 > /sys/devices/platform/i8042/serio1/serio2/speed
+echo -n 100 > /sys/devices/platform/i8042/serio1/serio2/speed
 echo -n 255 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
+echo -n 4 > /sys/devices/platform/i8042/serio1/serio2/inertia
