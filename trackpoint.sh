83e5e45f7749d82cf04548bc2790d37b305b8611
Cyril David <cyx@cyx.is>
2015-10-13 14:50:23 -0700

    Fix trackpoint script based on which serio is available

--- trackpoint.sh
+++ trackpoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo -n 100 > /sys/devices/platform/i8042/serio1/serio2/speed
-echo -n 255 > /sys/devices/platform/i8042/serio1/serio2/sensitivity
-echo -n 4 > /sys/devices/platform/i8042/serio1/serio2/inertia
+prefix=/sys/devices/platform/i8042/serio1
+
+test -e $prefix/serio3 && (
+	echo -n 100 > $prefix/serio3/speed &&
+	echo -n 255 > $prefix/serio3/sensitivity &&
+	echo -n 4 > $prefix/serio3/inertia
+) || (
+	echo -n 100 > $prefix/serio2/speed &&
+	echo -n 255 > $prefix/serio2/sensitivity &&
+	echo -n 4 > $prefix/serio2/inertia
+)
